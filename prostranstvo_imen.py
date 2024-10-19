1f77fa6ff508d021c16f99d323635a743c5ebca6
Vasil <95859124+dvasilhub@users.noreply.github.com>
2024-10-19 09:32:00 +0400

    Add files via upload

---
+++ prostranstvo_imen.py
@@ -0,0 +1,23 @@
+calls = 0
+
+# Создаём функцию count_calls и изменять в ней значение переменной calls.
+def count_calls ():
+    global calls
+    calls+=1
+
+# Создаём функцию string_info с параметром string (длина, верхний регистр и нижный)
+def string_info (string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+# Создаём функцию is_contains с двумя параметрами string и list_to_search (длина, верхний регистр и нижный)
+def is_contains (string, list_to_search):
+    count_calls()
+    return string.upper() in [s.upper() for s in list_to_search]
+
+# Вывод результата
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBan
+print(is_contains('cycle', ['recycle', 'cyclic'])) # No matches
+print(calls)
