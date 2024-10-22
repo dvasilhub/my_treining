6d6157041128551288241091d592831a45bb2448
Vasil <95859124+dvasilhub@users.noreply.github.com>
2024-10-22 12:05:34 +0400

    Add files via upload

--- recurciya.py
+++ recurciya.py
@@ -1,10 +1,20 @@
 def get_multiplied_digits(number):
+
     str_number = str(number)
-    if len(str_number) > 1:
+
+    if str_number[-1] == '0':
+        # Если да, убираем последнюю цифру
+        str_number = str_number[:-1]
+
+        # Если строка пустая после удаления, 0
+    if not str_number:
+        return 0
+
+    if len(str_number) > 1 :
         first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return int(str_number)
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(402030)
 print(result)
