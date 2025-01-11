47aa0905292b1147ec960d426798c2695b6f966a
Vasil <95859124+dvasilhub@users.noreply.github.com>
2025-01-11 11:15:24 +0400

    Add files via upload

--- Try и Except.py
+++ Try и Except.py
@@ -15,14 +15,11 @@
 def add_everything_up(a, b):
     try:
 
-        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-            return a + b
-
-        elif isinstance(a, str) and isinstance(b, str):
-            return a + b
-
+        if type(a) != type(b):
+            return str(a) + str(b)
         else:
-            raise TypeError
+            return a + b
+
     except TypeError:
 
         return f'{str(a)}{str(b)}'
@@ -31,3 +28,5 @@ def add_everything_up(a, b):
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))  
+
+
