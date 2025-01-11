a2c5c0e699e532ecf585dd87b4ebb210e6600967
Vasil <95859124+dvasilhub@users.noreply.github.com>
2025-01-11 11:49:33 +0400

    Add files via upload

---
+++ dekoratory.py
@@ -0,0 +1,24 @@
+# Домашнее задание по теме "Декораторы"
+# Задание: Декораторы в Python
+
+def is_prime(func):
+    def wrappper(*arf):
+        res = func(*arf)
+        prime = True
+        for i in range (2, res - 1):
+            if res % i == 0:
+                prime = False
+                break
+        if prime:
+            print('Простое')
+        else:
+            print('Составное')
+        return res
+    return wrappper
+
+@is_prime
+def sum_three(*args):
+    return sum(args)
+
+result = sum_three(2, 3, 6)
+print(result)
